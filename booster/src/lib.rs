ecdbb8939ffd9d892fa648104496082ae4d1c410
bikinkernel5 <putra0putri2@gmail.com>
2025-07-26 19:53:24 +0700

    Update lib.rs

--- booster/src/lib.rs
+++ booster/src/lib.rs
@@ -1,23 +1,15 @@
-// Deklarasi FFI ke Android log lib
-#[link(name = "log")]
-extern "C" {
-    fn __android_log_write(prio: i32, tag: *const i8, text: *const i8) -> i32;
-}
+use android_logger::Config;
+use log::Level;
 
-// Fungsi utama yang akan dipanggil dari Zygisk
 #[no_mangle]
 pub extern "C" fn zygisk_main() {
-    android_log("Zygisk Rust Booster aktif!");
-}
+    // Inisialisasi logger Android sekali saja
+    android_logger::init_once(
+        Config::default()
+            .with_min_level(Level::Info)
+            .with_tag("ZygiskRust"),
+    );
 
-// Fungsi logging ke logcat
-fn android_log(msg: &str) {
-    use std::ffi::CString;
-
-    let tag = CString::new("ZygiskRust").unwrap();
-    let c_msg = CString::new(msg).unwrap();
-
-    unsafe {
-        __android_log_write(4, tag.as_ptr(), c_msg.as_ptr());
-    }
+    // Log info ke logcat
+    log::info!("Zygisk Rust Booster aktif!");
 }
