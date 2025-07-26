bd2aa1364ba7f09c1dae9e4a3b47bd11b2a4a789
bikinkernel5 <putra0putri2@gmail.com>
2025-07-26 20:25:43 +0700

    Update lib.rs

--- booster/src/lib.rs
+++ booster/src/lib.rs
@@ -3,13 +3,11 @@ use log::Level;
 
 #[no_mangle]
 pub extern "C" fn zygisk_main() {
-    // Inisialisasi logger Android sekali saja
     android_logger::init_once(
         Config::default()
             .with_min_level(Level::Info)
             .with_tag("ZygiskRust"),
     );
 
-    // Log info ke logcat
     log::info!("Zygisk Rust Booster aktif!");
 }
